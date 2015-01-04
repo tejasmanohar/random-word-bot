0b50c63ddb791f7917c5c3e1065909b43211acec
Tejas Manohar <me@tejasmanohar.com>
2015-01-03 20:04:47 -0800

    define Twitter client

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 var request = require('superagent');
 var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+});
